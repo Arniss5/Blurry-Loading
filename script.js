a633150738045e7f80759e9b904861bbb99db64a
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2022-04-11 08:33:25 +0100

    Add preview

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let count = 0
 let blurCount = setInterval(blur, 30);
 
 //mapping percentage to opacity
+//(current num1, start of range for num1, end of range for num1, start range for num 2, end of range for num2) OUTPUT- current num2
 function scale (number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
@@ -19,13 +20,16 @@ function scale (number, inMin, inMax, outMin, outMax) {
 
 function blur() {
     count++
-    //if counter teached 100, clear interval ( clearInterval(name of interval))
+    //if counter reached 100, clear interval ( clearInterval(name of interval))
     if (count > 99) {
         clearInterval(blurCount)
     }
+    //set filter: blur() value
     bg.style.filter = `blur(${scale(count, 0, 100, 30, 0)}px)`;
 
-    percent.innerText = count;
+    //make percentage go from 0 to 100
+    percent.innerText = count +'%';
+    // set opacity
     percent.style.opacity = scale(count, 0, 100, 1, 0);
 
 }
